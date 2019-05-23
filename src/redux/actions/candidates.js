d5a9dc8c4085163d70d92d925d32ceab22f4666e
Martin Mutuma <12586916+martinMutuma@users.noreply.github.com>
2019-05-23 20:48:41 +0300

    Revert "Ft add redux"

--- src/redux/actions/candidates.js
+++
@@ -1,9 +0,0 @@
-import { actionTypes } from "../../actionTypes";
-import Candidates from "../../components/container/Candidates";
-
-export const storeListCandidates = candidates => {
-  return {
-    type: actionTypes.LIST_CANDIDATES,
-    payload: [...candidates]
-  };
-};
